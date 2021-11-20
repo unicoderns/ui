218fd54cb71285a4612e09d6fe5e9f1dce9f07e1
Jimmy Alberto Madrigal Cruz <jimmymadrigal+github@gmail.com>
2021-11-20 06:10:51 -0600

    modal

---
+++ src/components/ModalDialog/index.ts
@@ -0,0 +1,7 @@
+import ModalDialogComponent from './modal-dialog.component.vue'
+
+export { ModalDialogComponent }
+export * from './models/modal-dialog.model'
+export * from './models/modal-dialog-accessibility-config.model'
+export * from './models/modal-dialog-theme-config.model'
+export * from './types'
