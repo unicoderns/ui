78aa5e81d21c800c3ac94955acddc90aeb20fac4
jean.paniagua <paniagua.b.jean@gmail.com>
2021-12-09 11:39:27 -0600

    Installed highlight.js library

--- packages/code-higlight/src/ui-code-highlight.component.vue
+++ packages/code-higlight/src/ui-code-highlight.component.vue
@@ -2,15 +2,20 @@
   <div ref="code">
     <slot />
   </div>
+
+  <highlightjs language="html" code="console.log('Hello World');" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
 
 const TAG_NAME = 'uiCodeHighlight'
-
 export default defineComponent({
   TAG_NAME,
+  components: {
+    highlightjs: hljsVuePlugin.component,
+  },
   setup(props, { emit, attrs }) {
     const code = ref('')
     onMounted(() => {
