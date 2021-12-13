a09177363faa999e980c516d3b81d3aa889cfac1
jean.paniagua <paniagua.b.jean@gmail.com>
2021-12-13 14:42:58 -0600

    installed prism library and correct highlight

--- packages/code-higlight/src/ui-code-highlight.component.vue
+++ packages/code-higlight/src/ui-code-highlight.component.vue
@@ -1,14 +1,18 @@
 <template>
-  <div ref="code">
+  <!-- <div ref="code">
     <slot />
-  </div>
-
-  <highlightjs language="html" code="console.log('Hello World');" />
+  </div> -->
+  <!-- <highlightjs language="html" :code="html" /> -->
+  <pre><code class="language-html">{{html}}</code></pre>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
 import hljsVuePlugin from '@highlightjs/vue-plugin'
+import 'highlight.js/styles/stackoverflow-light.css'
+import hljs from 'highlight.js/lib/core'
+import Prism from 'prismjs'
+import 'prismjs/themes/prism.css'
 
 const TAG_NAME = 'uiCodeHighlight'
 export default defineComponent({
@@ -18,11 +22,21 @@ export default defineComponent({
   },
   setup(props, { emit, attrs }) {
     const code = ref('')
+    const html = `<div><ul class="nav nav-tabs"><li class="nav-item"><a class="nav-link
+          
+          " role="tab">Tab 1</a></li><li class="nav-item"><a class="nav-link
+          
+          " role="tab">Tab 2</a></li><li class="nav-item"><a class="nav-link
+          active
+          " aria-current="page" role="tab">Tab 3</a></li><li class="nav-item"><a class="nav-link
+          
+          disabled" role="tab">Tab 4</a></li></ul><!--v-if--><!--v-if--> Hello From Tab 3 <!--v-if--></div>`
     onMounted(() => {
+      Prism.highlightAll()
       console.log(code.value)
     })
 
-    return { code }
+    return { code, html }
   },
 })
 </script>
