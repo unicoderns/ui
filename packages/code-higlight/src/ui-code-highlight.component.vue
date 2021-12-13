eb0d0b5bd2199a1f08e1946df5b14348510457db
jean.paniagua <paniagua.b.jean@gmail.com>
2021-12-13 16:25:19 -0600

    Added line number

--- packages/code-higlight/src/ui-code-highlight.component.vue
+++ packages/code-higlight/src/ui-code-highlight.component.vue
@@ -1,42 +1,35 @@
 <template>
-  <!-- <div ref="code">
+  <div ref="code">
     <slot />
-  </div> -->
-  <!-- <highlightjs language="html" :code="html" /> -->
-  <pre><code class="language-html">{{html}}</code></pre>
+  </div>
+  <pre class="line-numbers"><code class="language-html">{{newText}}</code></pre>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
-import hljsVuePlugin from '@highlightjs/vue-plugin'
-import 'highlight.js/styles/stackoverflow-light.css'
-import hljs from 'highlight.js/lib/core'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism.css'
+import 'prismjs/components/prism-markdown'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 const TAG_NAME = 'uiCodeHighlight'
 export default defineComponent({
   TAG_NAME,
-  components: {
-    highlightjs: hljsVuePlugin.component,
-  },
   setup(props, { emit, attrs }) {
     const code = ref('')
     const html = `<div><ul class="nav nav-tabs"><li class="nav-item"><a class="nav-link
-          
           " role="tab">Tab 1</a></li><li class="nav-item"><a class="nav-link
-          
           " role="tab">Tab 2</a></li><li class="nav-item"><a class="nav-link
           active
           " aria-current="page" role="tab">Tab 3</a></li><li class="nav-item"><a class="nav-link
-          
           disabled" role="tab">Tab 4</a></li></ul><!--v-if--><!--v-if--> Hello From Tab 3 <!--v-if--></div>`
+    const newText = html.replace(/^[\r\n\s]*|[\r\n\s]*$/g, '')
     onMounted(() => {
       Prism.highlightAll()
       console.log(code.value)
     })
 
-    return { code, html }
+    return { code, html, newText }
   },
 })
 </script>
