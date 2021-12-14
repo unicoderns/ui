4542a5b02fdd2da290ec298d4dfb35af12c0d244
Alejandro Navarro <alejandro.navarro@alejandro-navarro-MacBook-Pro.local>
2021-12-14 17:29:55 -0500

    Added alert's unit tests
    - Added close button unit tests

--- packages/alert/tests/unit/ui-alert.spec.ts
+++ packages/alert/tests/unit/ui-alert.spec.ts
@@ -3,6 +3,8 @@ import { BootstrapVariants } from '@unicodernsui/core'
 import { UiAlert } from '../../src'
 import { bsUiAlertThemeConfigDefaults } from '../../src/defaults/bs-ui-alert-theme.config'
 
+const theme = bsUiAlertThemeConfigDefaults
+
 describe('ui-alert.vue', () => {
   it('renders when props are set', () => {
     const wrapper = shallowMount(UiAlert, {
@@ -12,7 +14,7 @@ describe('ui-alert.vue', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
 
-  it('displays text as slot', () => {
+  it('should displays text as slot', () => {
     const defaultText = 'Default text'
     const wrapper = mount(UiAlert, {
       slots: { default: defaultText },
@@ -20,19 +22,64 @@ describe('ui-alert.vue', () => {
     expect(wrapper.html()).toContain(defaultText)
   })
 
-  it('should be dismissible', () => {
-    const theme = bsUiAlertThemeConfigDefaults
+  it('should have dismissible class when dismissible prop is true', () => {
     const wrapper = mount(UiAlert, {
       props: {
         dismissible: true,
       },
     })
 
-    expect(wrapper.findComponent({ name: 'UiButtonClose' }))
+    expect(wrapper.findComponent({ name: 'UiButtonClose' })).toBeTruthy()
     expect(wrapper.find('div').classes()).toContain(theme.cssClass.dismissible)
   })
 
-  it('should emit close alert', () => {
+  it('should have animate class when prop animate is true', () => {
+    const wrapper = mount(UiAlert, {
+      props: {
+        animate: true,
+      },
+    })
+
+    expect(wrapper.find('div').classes()).toContain(theme.cssClass.animated)
+  })
+
+  it('should not be displayed when show prop is setted false', () => {
+    const wrapper = mount(UiAlert, {
+      props: {
+        show: false,
+      },
+    })
+
+    expect(wrapper.find('div').exists()).toBeFalsy()
+  })
+
+  it('should render custom accesibility role when props.role is set', () => {
+    const role = 'some role'
+
+    const wrapper = mount(UiAlert, {
+      props: { ['aria:role']: role, variant: BootstrapVariants.Primary },
+    })
+
+    const div = wrapper.find('div')
+    expect(div.attributes().role).toEqual(role)
+  })
+
+  it('should render custom accesibility label when props.label is set', () => {
+    const buttonClose = 'some buttonClose'
+
+    const wrapper = mount(UiAlert, {
+      props: {
+        dismissible: true,
+        ['aria:buttonClose']: buttonClose,
+        variant: BootstrapVariants.Primary,
+      },
+    })
+
+    const button = wrapper.find('button')
+    expect(button.attributes('aria-label')).toEqual(buttonClose)
+  })
+
+  it('should emit close action with true when props.dismissible is true', () => {
     const wrapper = mount(UiAlert, {
       props: {
         dismissible: true,
@@ -41,5 +88,23 @@ describe('ui-alert.vue', () => {
 
     wrapper.vm.$emit('close')
     expect(wrapper.emitted().close).toBeTruthy()
+  })
+
+  it('should emit show action with true', () => {
+    const wrapper = mount(UiAlert, {
+      props: {},
+    })
+
+    wrapper.vm.$emit('show')
+    expect(wrapper.emitted().show).toBeTruthy()
+  })
+
+  it('should emit hide action with true', () => {
+    const wrapper = mount(UiAlert, {
+      props: {},
+    })
+
+    wrapper.vm.$emit('hide')
+    expect(wrapper.emitted().hide).toBeTruthy()
   })
 })
