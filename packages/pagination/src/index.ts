7f8bef8401e9d75b5d1d73d240bff69140b05d45
Alejandro Saldarriaga <alejandroasp@gmail.com>
2022-03-07 08:44:30 -0500

    review01 - remove commented lines

--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -1,9 +1,6 @@
 import UiPagination from './ui-pagination.component.vue'
 import UiPaginationItem from './ui-pagination-item.component.vue'
 
-// UiPagination.TAG_NAME = 'uiPagination'
-// UiPaginationItem.TAG_NAME = 'uiPaginationItem'
-
 export { UiPagination, UiPaginationItem }
 export * from './models/ui-pagination.model'
 export * from './models/ui-pagination-theme-config.model'
