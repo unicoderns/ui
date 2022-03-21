9a4cc7b3e0309da64b51236a725cf7d58637370e
Jimmy Alberto Madrigal Cruz <jimmymadrigal+github@gmail.com>
2022-03-20 19:30:14 -0600

    move button close component to script setup

--- packages/button-close/src/ui-button-close.component.vue
+++ packages/button-close/src/ui-button-close.component.vue
@@ -8,54 +8,50 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, toRefs } from 'vue'
+const TAG_NAME = 'uiButtonClose'
+export default { TAG_NAME }
+</script>
+
+<script setup lang="ts">
+import { computed, useAttrs, toRefs } from 'vue'
 import { useReactiveAriaConfig, useReactiveThemeConfig } from '@uicr/core'
 import { uiButtonCloseThemeConfigDefaults } from './defaults/ui-button-close-theme.config'
 import { UiButtonCloseAriaConfigModel } from './models/ui-button-close-aria-config.model'
 import { uiButtonCloseAriaDefaults } from './defaults/ui-button-close-aria.config'
 import { UiButtonCloseThemeConfigModel } from './models/ui-button-close-theme-config.model'
 
-const TAG_NAME = 'uiButtonClose'
-
-export default defineComponent({
-  TAG_NAME,
-  props: {
-    invert: { type: Boolean, default: false },
-    ['aria:label']: { type: String, default: null },
-  },
-  emits: ['close'],
-  setup(props, { emit, attrs }) {
-    const { invert } = toRefs(props)
-
-    const theme = useReactiveThemeConfig<UiButtonCloseThemeConfigModel>(
-      TAG_NAME,
-      attrs,
-      props,
-      uiButtonCloseThemeConfigDefaults
-    )
-    const aria = useReactiveAriaConfig<UiButtonCloseAriaConfigModel>(
-      TAG_NAME,
-      attrs,
-      props,
-      uiButtonCloseAriaDefaults
-    )
-
-    const classes = computed((): string[] => {
-      return [
-        theme.value.cssClass.main,
-        invert.value ? theme.value.cssClass.inverted : '',
-      ]
-    })
-
-    const close = (): void => {
-      emit('close')
-    }
-
-    return {
-      classes,
-      close,
-      aria,
-    }
-  },
+const props = defineProps({
+  invert: { type: Boolean, default: false },
+  ['aria:label']: { type: String, default: null },
 })
+
+const emit = defineEmits(['close'])
+
+const attrs = useAttrs()
+
+const { invert } = toRefs(props)
+
+const theme = useReactiveThemeConfig<UiButtonCloseThemeConfigModel>(
+  TAG_NAME,
+  attrs,
+  props,
+  uiButtonCloseThemeConfigDefaults
+)
+const aria = useReactiveAriaConfig<UiButtonCloseAriaConfigModel>(
+  TAG_NAME,
+  attrs,
+  props,
+  uiButtonCloseAriaDefaults
+)
+
+const classes = computed((): string[] => {
+  return [
+    theme.value.cssClass.main,
+    invert.value ? theme.value.cssClass.inverted : '',
+  ]
+})
+
+const close = (): void => {
+  emit('close')
+}
 </script>
