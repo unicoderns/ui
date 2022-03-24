e3a3c7a8f670343ed9b5fc453057586790a1c8c1
Jimmy Alberto Madrigal Cruz <jimmymadrigal+github@gmail.com>
2022-03-23 19:50:37 -0600

    button close tailwind migration

--- packages/button-close-tw/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { setComponentThemeConfig } from '@uicr/core'
-import { UiButtonClose } from '@uicr/button-close'
-import { twUiButtonCloseThemeConfigDefaults } from './tw-ui-button-close-theme.config'
-
-export function install() {
-  setComponentThemeConfig(UiButtonClose, twUiButtonCloseThemeConfigDefaults)
-}
-
-export { twUiButtonCloseThemeConfigDefaults }
